3cfa47dffb289ab15f10b811f85df24ba06395ed
pontura <pontura@gmail.com>
2016-12-13 18:39:17 -0300

    popup

---
+++ cabildo/Assets/GloboCasa.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GloboCasa : MonoBehaviour {
+    
+	void Start () {
+	
+	}
+}
--- cabildo/Assets/GlobosManager.cs
+++ cabildo/Assets/GlobosManager.cs
@@ -6,13 +6,17 @@ public class GlobosManager : MonoBehaviour {
     public GloboInfo globoSimple;
     public GloboMultipleChoice globoMultipleChoice;
     public GloboFoto globoFoto;
-    
-    private Texts texts;    
+    public GloboCasa globoCasa;
+
+    private Texts texts;
+    private Vector2 globoCasa_pos;
 
     void Start() {
+        globoCasa_pos = globoCasa.transform.position;
         texts = Data.Instance.texts;
         Events.OnGloboDialogo += OnGloboDialogo;
         Events.OnClickOutside += OnClickOutside;
+        ResetGlobos();
     }
     void OnClickOutside()
     {
@@ -24,25 +28,29 @@ public class GlobosManager : MonoBehaviour {
         globoSimple.transform.position = pos;
         globoMultipleChoice.transform.position = pos;
         globoFoto.transform.position = pos;
+        globoCasa.transform.position = pos;
     }
     void OnGloboDialogo(Vector3 pos, string text)
     {
         ResetGlobos();
         Texts.types type = texts.GetTypeOfContent(text);
+        switch(text)
+        {
+            case "casaCheta":
+                globoCasa.transform.position = globoCasa_pos;
+                break;
+        }
         switch (type)
         {
-            case Texts.types.SIMPLE:
-                globoSimple.transform.position = pos;
-                globoSimple.Init(text);
-                break;
-
+            //case Texts.types.SIMPLE:
+            //    globoSimple.transform.position = pos;
+            //    globoSimple.Init(text);
+            //    break;
             case Texts.types.MULTIPLECHOICE:
                 globoMultipleChoice.transform.position = pos;
                 globoMultipleChoice.Init(text);
                 break;
-
         }
-
         print("OnGloboDialogo pos: " + pos + "    text: " + text + " type: " + type);
 
     }
